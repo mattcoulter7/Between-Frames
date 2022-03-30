941983269c05b3637edeea854b78a62d384eb7bf
Matthew Coulter <mattcoul7@gmail.com>
2022-03-30 14:23:41 +1100

    Added resizing of individual bar

--- camera-game/Assets/CinematicBarController.cs
+++ camera-game/Assets/CinematicBarController.cs
@@ -8,33 +8,58 @@ public class CinematicBarController : MonoBehaviour
     public float rotateSpeed = 1f;
     private CinematicBars _cinematicBars;
     private Vector3 _lastMousePos = new Vector3(0f,0f,0f);
+
+    private Vector3 _mousePos = new Vector3(0f,0f,0f);
+    private Vector3 _mouseMovement = new Vector3(0f,0f,0f);
     // Start is called before the first frame update
     void Start()
     {
         _cinematicBars = GetComponent<CinematicBars>();
     }
 
+    CinematicBar GetMouseInteractCinematicBar(){
+        CinematicBar cinematicBar = null;
+        Ray ray = Camera.main.ScreenPointToRay(_mousePos);
+        if (Physics.Raycast(ray,out RaycastHit hitInfo)){
+            CinematicBar barComponent = hitInfo.collider.gameObject.GetComponent<CinematicBar>();
+            if (barComponent){
+                cinematicBar = barComponent;
+            }
+        }
+        return cinematicBar;
+    }
+
     // Update is called once per frame
     void Update()
     {
         // get mouse pos
-        Vector3 mousePos = Input.mousePosition;
-        Vector3 mouseMovement = _lastMousePos - mousePos;
+        _mousePos = Input.mousePosition;
+        _mouseMovement = _lastMousePos - _mousePos;
         // handle zooming to change distance
+        
         float scrollAmount = Input.mouseScrollDelta.y;
-        _cinematicBars.distance += scrollAmount * zoomSpeed;
+        if (scrollAmount != 0f){
+            // find object mouse is intersecting
+            CinematicBar bar = GetMouseInteractCinematicBar();
+            if (bar == null){
+                // if it is one of the black bars, chane that instead of both
+                _cinematicBars.distance += scrollAmount * zoomSpeed;
+            } else {
+                bar.distanceOffset += scrollAmount * zoomSpeed;
+            }
+        }
 
         // click to move origin
         if (Input.GetMouseButton(0)){
-            _cinematicBars.offset -= (Vector2)Camera.main.ScreenToViewportPoint(mouseMovement);;
+            _cinematicBars.offset -= (Vector2)Camera.main.ScreenToViewportPoint(_mouseMovement);;
         }
 
         // move mouse right whilst holding left mouse button to rotate
         if (Input.GetMouseButton(1)){
-            _cinematicBars.rotation += mouseMovement.x * rotateSpeed;
+            _cinematicBars.rotation += _mouseMovement.x * rotateSpeed;
         }
 
         // track last mouse position
-        _lastMousePos = mousePos;
+        _lastMousePos = _mousePos;
     }
 }
--- camera-game/Assets/CinematicBars.cs
+++ camera-game/Assets/CinematicBars.cs
@@ -6,6 +6,19 @@ public class CinematicBars : MonoBehaviour
 {
     public bool maintainPlayableArea = false;
     public float rotation = 0f;
-    public Vector2 offset = new Vector2(0.5f,0.5f);
+    public Vector2 _offset = new Vector2(0.5f,0.5f);
+    public Vector2 offset {
+        get {
+            return _offset;
+        }
+        set {
+            // ensure origin is always in the screen
+            value.x = Mathf.Max(value.x,0);
+            value.x = Mathf.Min(value.x,1);
+            value.y = Mathf.Max(value.y,0);
+            value.y = Mathf.Min(value.y,1);
+            _offset = value;
+        }
+    }
     public float distance = 0f;
 }
