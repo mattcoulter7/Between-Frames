ddb93be2d58f25e5e6787fe057b3cff15658b551
Goshen Telan <102566669@student.swin.edu.au>
2022-04-08 11:23:09 +1000

    adding Goshen's scene
    adding my own playground to test things- Goshen

---
+++ camera-game/Assets/tryMove.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+ 
+ public class playerMovement : MonoBehaviour {
+     
+     public float moveSpeed;
+     public float jumpHeight;
+ 
+     // Use this for initialization
+     void Start () {
+ 
+     }
+     
+     // Update is called once per frame
+     void Update () {
+ 
+     }
+ 
+     void FixedUpdate () {
+         if (Input.GetKey (KeyCode.W)){
+             GetComponent<Rigidbody>().velocity = new Vector2(0, jumpHeight);
+         }else if (Input.GetKey(KeyCode.A)){
+             GetComponent<Rigidbody>().velocity = new Vector2(-moveSpeed, 0);
+         }else if (Input.GetKey (KeyCode.D)){
+             GetComponent<Rigidbody>().velocity = new Vector2(moveSpeed, 0);
+         }
+     }
+ } 
