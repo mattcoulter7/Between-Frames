d6452b8d698f2013edfc511330c06bd504e0a684
Matthew Coulter <mattcoul7@gmail.com>
2022-04-17 13:24:50 +1000

    Added Player Model

--- camera-game/Assets/Scripts/Layering/LockTransform.cs
+++ camera-game/Assets/Scripts/Layering/LockTransform.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class LockTransform : MonoBehaviour
 {
     public Vector3 position;
-    public bool[] lockAxis = new bool[3]{false,false,false};
+    public bool[] lockPositionAxis = new bool[3]{false,false,false};
+    public Vector3 rotation;
+    public bool[] lockRotationAxis = new bool[3]{false,false,false};
+    public Vector3 scale;
+    public bool[] lockScaleAxis = new bool[3]{false,false,false};
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +20,21 @@ public class LockTransform : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(
-            lockAxis[0] ? position.x : transform.position.x,
-            lockAxis[1] ? position.y : transform.position.y,
-            lockAxis[2] ? position.z : transform.position.z
+            lockPositionAxis[0] ? position.x : transform.position.x,
+            lockPositionAxis[1] ? position.y : transform.position.y,
+            lockPositionAxis[2] ? position.z : transform.position.z
+        );
+
+        transform.rotation = Quaternion.Euler(
+            lockRotationAxis[0] ? rotation.x : transform.rotation.x,
+            lockRotationAxis[1] ? rotation.y : transform.rotation.y,
+            lockRotationAxis[2] ? rotation.z : transform.rotation.z
+        );
+
+        transform.localScale = new Vector3(
+            lockScaleAxis[0] ? scale.x : transform.localScale.x,
+            lockScaleAxis[1] ? scale.y : transform.localScale.y,
+            lockScaleAxis[2] ? scale.z : transform.localScale.z
         );
     }
 }
