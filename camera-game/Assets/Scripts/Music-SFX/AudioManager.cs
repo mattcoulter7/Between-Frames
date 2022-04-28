9a787b7389b0d96003674d42d2dc2046af12e8cd
Goshen Telan <102566669@student.swin.edu.au>
2022-04-28 13:35:04 +1000

    music/sfx added audio manager

---
+++ camera-game/Assets/Scripts/Music-SFX/AudioManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine.Audio;
+using System.Collections;
+using UnityEngine;
+using System;
+
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject); 
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            //s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+
+        Play("BGM");
+        Play("Water");
+        Play("Wind");
+    }
+
+    public void Play(string name)
+    {
+
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "not found");
+            return;
+        }
+        s.source.Play();
+    }
+}
