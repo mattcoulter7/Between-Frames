1474c4c678de301d4e7d961b7ad417e4f7912d6b
Tony Lam <65806916+ku1a@users.noreply.github.com>
2022-05-03 15:33:03 +1000

    char animations + new scene
    character animation script + test scene separate from others.

---
+++ camera-game/Assets/Animation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetButton("Horizontal"))
+        {
+            anim.SetBool("isWalking", true);
+
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }*/
+
+        anim.SetBool("isWalking", Input.GetButton("Horizontal"));
+    }
+}
