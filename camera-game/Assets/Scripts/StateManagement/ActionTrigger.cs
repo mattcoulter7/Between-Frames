dccaaded4546d27f1dab7d2e85eb5d388feba23b
Goshen Telan <102566669@student.swin.edu.au>
2022-05-12 13:16:36 +1000

    Reset button added

---
+++ camera-game/Assets/Scripts/StateManagement/ActionTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionTrigger : Trigger
+{
+    public string actionString;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+
+        if (Input.GetAxis(actionString) != 0){
+
+            OnTriggerStart();
+
+        }
+
+         
+    }
+}
