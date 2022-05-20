35ed6af61643915e6d1048b0344b5e4a4056347d
Goshen Telan <102566669@student.swin.edu.au>
2022-05-20 11:11:28 +1000

    DEMO READY

--- camera-game/Assets/Scripts/Music-SFX/PlaySound.cs
+++ camera-game/Assets/Scripts/Music-SFX/PlaySound.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class PlaySound : MonoBehaviour
 {
+    private bool startedPlaying = false;
+
     public void Play(string name)
     {
 
         AM.Instance.PlaySFX(name);
     }
-    
+
+    public void PlayLooped(string name)
+    {
+        if (!startedPlaying)
+        {
+            AM.Instance.PlaySFX(name);
+            startedPlaying = true;
+        }
+        else
+        {
+            return;
+        }
+       
+    }
+
+    public void StopLooped(string name)
+    {
+        startedPlaying = false;
+        AM.Instance.StopSFX(name);
+
+    }
+
 }
