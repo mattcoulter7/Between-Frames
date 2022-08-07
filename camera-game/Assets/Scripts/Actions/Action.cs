c466e8db8ec1e96d9d90994b285a561fcfa385fe
Jackostar <101544626+Jackostar@users.noreply.github.com>
2022-08-07 22:29:28 +1000

    Possibly replicated the fix on the correct version of poiyomi

--- camera-game/Assets/Scripts/Actions/Action.cs
+++ camera-game/Assets/Scripts/Actions/Action.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,4 +25,8 @@ public abstract class Action : MonoBehaviour
         OnRun();
     }
 
+    public void Invoke()
+    {
+        throw new NotImplementedException();
+    }
 }
