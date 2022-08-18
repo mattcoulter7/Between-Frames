ada43eb174d18ee0928a35fc7bbb70ecbc98b9fe
Matthew Coulter <53892067+mattcoulter7@users.noreply.github.com>
2022-08-18 16:14:29 +1000

    Documentation for old rewind system

--- camera-game/Assets/Scripts/Rewind/AnimationEventRecord.cs
+++ camera-game/Assets/Scripts/Rewind/AnimationEventRecord.cs
@@ -5,69 +5,52 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
-// class used for sending custom data through animation callbacks
-[CreateAssetMenu(fileName = "CustomAnimationEventMessage", menuName = "Custom Animation Event Message", order = 51)]
-public class CustomAnimationEventMessage : ScriptableObject
-{
-    public DynamicModifier valueSetter;
-    public object value
-    {
-        get
-        {
-            return _value;
-        }
-        set
-        {
-            _value = value;
-            try
-            {
-                _boolValue = (bool)value;
-            }
-            catch (InvalidCastException e) { }
-            try
-            {
-                _stringValue = (string)value;
-            }
-            catch (InvalidCastException e) { }
-            try
-            {
-                _intValue = (int)value;
-            }
-            catch (InvalidCastException e) { }
-            try
-            {
-                _floatValue = (float)value;
-            }
-            catch (InvalidCastException e) { }
-        }
-    }
 
-    // debug properties
-    private object _value;
-    private bool _boolValue;
-    private string _stringValue;
-    private int _intValue;
-    private float _floatValue;
-}
-
-// OBSERVES FUNCTION CHANGES
+/// <summary>
+/// This class is similar to the Animation Record, but instead of recording only float values, 
+/// can set values of other types, and trigger function calls
+/// </summary>
 [System.Serializable]
 public class AnimationEventRecord : AnimationRecord
 {
-    public DynamicModifier valueSetter; // Used for setting the value property
-    public string functionName; // function called to update variable
+    /// <summary>
+    /// Used for setting the value property
+    /// </summary>
+    public DynamicModifier valueSetter;
+
+    /// <summary>
+    /// function called to update variable
+    /// </summary>
+    public string functionName;
+
+    /// <summary>
+    /// List of AnimationEvents used for storing the AnimationEvent history
+    /// </summary>
     public List<AnimationEvent> animationEvents;
 
+    /// <summary>
+    /// Returns the last value saved into the aniamtion event history, or null if the history is empty
+    /// </summary>
     public override object lastValue => animationEvents.Count > 0 ? ((CustomAnimationEventMessage)(animationEvents[animationEvents.Count - 1].objectReferenceParameter)).value : null;
+
+    /// <summary>
+    /// The number of animation events that have been saved
+    /// </summary>
     public override int frameCount => animationEvents.Count;
 
-    // Update is called once per frame
+    /// <summary>
+    /// Ensures all of the DynamicModifiers are ready to be used
+    /// </summary>
     public override void OnInitialise()
     {
         base.OnInitialise();
         valueSetter.OnInitialise();
     }
 
+    /// <summary>
+    /// Records a single AnimationEvent keyframe
+    /// </summary>
+    /// <param name="optimise">true will only save the keyframe if the value has changed</param>
     public override void Record(bool optimise = true)
     {
         // list to values from a given path (could be a variable or a method that returns something)
@@ -84,11 +67,22 @@ public class AnimationEventRecord : AnimationRecord
         animationEvents.Add(CreateAnimationEvent(value));
     }
 
+    /// <summary>
+    /// Saves the cached animationEvents into the AnimationClip
+    /// </summary>
+    /// <param name="clip">the AnimationClip in which the cache will be saved to</param>
     public override void Apply(AnimationClip clip)
     {
         clip.events = animationEvents.ToArray();
     }
 
+    /// <summary>
+    /// Generates a prepared AnimationEvent which is already linked with the OnEventFire callback
+    /// OnEventFire will handle calling the custom setter DynamicObject
+    /// </summary>
+    /// <param name="value">The value passed into the mediator DynamicObject which will be set</param>
+    /// <param name="timeOffset">An offset from the current time, used for saving a keyframe right before the actual keyframe to avoid weird interpolation</param>
+    /// <returns>The AnimationEvent</returns>
     private AnimationEvent CreateAnimationEvent(object value,float timeOffset = 0f)
     {
         AnimationEvent e;
