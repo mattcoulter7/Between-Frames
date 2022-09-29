09d1bcfa3d60ac8292c480ed4607514112ce1479
Matthew Coulter <mattcoul7@gmail.com>
2022-09-30 01:25:02 +1000

    Added layers to StateMachine

--- camera-game/Assets/Scripts/StateManagement/StateMachine.cs
+++ camera-game/Assets/Scripts/StateManagement/StateMachine.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Events;
 
 public class StateMachine : MonoBehaviour
 {
     public string defaultState;
-    public List<State> history = new List<State> { null };
+    public List<StateMachineLayer> layers = new List<StateMachineLayer> { };
+    private List<State> history = new List<State> { null };
+    private string selectedLayer = "";
     public State currentState
     {
         get
@@ -19,62 +23,120 @@ public class StateMachine : MonoBehaviour
             history[0] = value;
         }
     }
-    Dictionary<string, State> states = new Dictionary<string, State>();
-    void Start()
+    private Dictionary<string, State> states = new Dictionary<string, State>();
+    public void SelectLayer(string layer)
     {
-        if (defaultState != "") ChangeState(defaultState);
+        selectedLayer = layer;
+    }
+    public void ChangeState(string state) // overrides the current state
+    {
+        if (selectedLayer == "")
+        {
+            State stateObj = state == "" ? null : states[state];
+            ChangeState(stateObj);
+        } 
+        else
+        {
+            StateMachineLayer layerObj = GetStateMachineLayer(selectedLayer);
+            layerObj.ChangeState(state);
+            selectedLayer = "";
+        }
     }
 
-    void Update()
+    public void AddState(string state) // adds to front of history
+    {
+        if (selectedLayer == "")
+        {
+            if (currentState != null) currentState.Exit();
+
+            State stateObj = states[state];
+            if (currentState == null) currentState = stateObj;
+            else history.Insert(0, stateObj);
+
+            currentState.Enter();
+        }
+        else
+        {
+            StateMachineLayer layerObj = GetStateMachineLayer(selectedLayer);
+            layerObj.ChangeState(state);
+            selectedLayer = "";
+        }
+    }
+
+    public void RemoveState() // removes from of history
+    {
+        if (selectedLayer == "")
+        {
+            if (currentState != null)
+            {
+                currentState.Exit();
+                history.RemoveAt(0);
+
+                if (currentState != null) currentState.Enter();
+            }
+        }
+        else
+        {
+            StateMachineLayer layerObj = GetStateMachineLayer(selectedLayer);
+            layerObj.RemoveState();
+            selectedLayer = "";
+        }
+    }
+
+    public void RegisterState(string value, State obj)
+    {
+        if (selectedLayer == "")
+        {
+            states[value] = obj;
+        }
+        else
+        {
+            StateMachineLayer layerObj = GetStateMachineLayer(selectedLayer);
+            layerObj.RegisterState(value,obj);
+            selectedLayer = "";
+        }
+    }
+    private void Start()
+    {
+        if (defaultState != "") ChangeState(defaultState);
+        foreach (StateMachineLayer layer in layers)
+        {
+            layer.Start();
+        }
+    }
+
+
+    private void Update()
     {
         if (currentState != null) currentState.HandleInput();
         if (currentState != null) currentState.HandleShouldChangeState();
         if (currentState != null) currentState.LogicUpdate();
+        foreach (StateMachineLayer layer in layers)
+        {
+            layer.Update();
+        }
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         if (currentState != null) currentState.PhysicsUpdate();
+        foreach (StateMachineLayer layer in layers)
+        {
+            layer.FixedUpdate();
+        }
     }
-
-    public void ChangeState(State stateObj) // overrides the current state
+    private StateMachineLayer GetStateMachineLayer(string name)
+    {
+        StateMachineLayer layer = layers.Find(x => x.name == name);
+        if (layer == null) throw new Exception("Layer '" + name + "' does not exist on " + gameObject.name);
+        return layer;
+    }
+    private void ChangeState(State stateObj) // overrides the current state
     {
         if (currentState == stateObj) return; // can't re-enter the current state
 
         if (currentState != null) currentState.Exit();
         currentState = stateObj;
         if (currentState != null) currentState.Enter();
-    }
-    public void ChangeState(string state) // overrides the current state
-    {
-        State stateObj = state == "" ? null : states[state];
-        ChangeState(stateObj);
-    }
-
-    public void AddState(string state) // adds to front of history
-    {
-        if (currentState != null) currentState.Exit();
-
-        State stateObj = states[state];
-        if (currentState == null) currentState = stateObj;
-        else history.Insert(0, stateObj);
-
-        currentState.Enter();
-    }
-
-    public void RemoveState() // removes from of history
-    {
-        if (currentState != null)
-        {
-            currentState.Exit();
-            history.RemoveAt(0);
-
-            if (currentState != null) currentState.Enter();
-        }
-    }
-
-    public void RegisterState(string value, State obj)
-    {
-        states[value] = obj;
     }
 }
