09aa3caba077e3921a49d6371dde891b5c9ad795
Matthew Coulter <mattcoul7@gmail.com>
2022-10-03 14:41:54 +1100

    Added skip button on cutscene

--- camera-game/Assets/Scripts/Video/Cutscene.cs
+++ camera-game/Assets/Scripts/Video/Cutscene.cs
@@ -1,23 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 
 public class Cutscene : MonoBehaviour
 {
     public string nextScene;
+    public GameObject skipButton;
+    public float skipbuttonShowDuration = 5f;
+
     private VideoPlayer video;
+    private PlayerInput playerInput;
 
     private void Awake()
     {
+        playerInput = FindObjectOfType<PlayerInput>();
+        playerInput.SwitchCurrentActionMap("UI");
+        playerInput.actions["Submit"].performed += ShowSkipButton;
+
         video = GetComponent<VideoPlayer>();
         video.Play();
-        video.loopPointReached += CheckOver;
+        video.loopPointReached += OnCutsceneEnd;
     }
 
-    private void CheckOver(VideoPlayer vp)
+    private void OnCutsceneEnd(VideoPlayer vp)
     {
-        SceneManager.LoadScene(nextScene); //the scene to load after video finishes
+        SceneManager.LoadScene(nextScene);
+    }
+    public void ShowSkipButton(InputAction.CallbackContext context)
+    {
+        skipButton.SetActive(true);
+        playerInput.actions["Submit"].performed -= ShowSkipButton;
+        playerInput.actions["Submit"].performed += OnSkip;
+        StartCoroutine(HideSkipButton());
+    }
+
+    public IEnumerator HideSkipButton()
+    {
+        yield return new WaitForSeconds(skipbuttonShowDuration);
+
+        skipButton.SetActive(false);
+        playerInput.actions["Submit"].performed -= OnSkip;
+        playerInput.actions["Submit"].performed += ShowSkipButton;
+    }
+
+    public void OnSkip(InputAction.CallbackContext context)
+    {
+        playerInput.actions["Submit"].performed -= OnSkip;
+        OnCutsceneEnd(video);
     }
 }
