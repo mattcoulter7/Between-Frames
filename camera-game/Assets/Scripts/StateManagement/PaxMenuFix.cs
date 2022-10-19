d6e39a59a3ec5711594f8ebc0a49f01a114f4894
DavudNG <brokenarma@gmail.com>
2022-10-19 23:14:35 +1100

    Fix for Animators not playing in main menu

---
+++ camera-game/Assets/Scripts/StateManagement/PaxMenuFix.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaxMenuFix : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+}
