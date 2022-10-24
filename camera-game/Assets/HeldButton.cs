e4aab058d88ffbff85e87af1bf7fd84002b77160
Matthew Coulter <mattcoul7@gmail.com>
2022-10-24 13:06:33 +1100

    Held Button Value is working

--- camera-game/Assets/HeldButton.cs
+++ camera-game/Assets/HeldButton.cs
@@ -7,6 +7,7 @@ using UnityEngine.InputSystem;
 
 public class HeldButton : MonoBehaviour
 {
+    public float currentValue = 0f;
     public float holdTime = 2f;
     public string inputBinding;
     public Action performed;
@@ -16,6 +17,7 @@ public class HeldButton : MonoBehaviour
 
     private HeldPlayerInput heldPlayerInput;
     private Coroutine heldTimer = null;
+    private Coroutine valueCoroutine = null;
     // Start is called before the first frame update
     void Awake()
     {
@@ -37,24 +39,60 @@ public class HeldButton : MonoBehaviour
     private void OnHoldBegin(InputAction.CallbackContext ctx)
     {
         float value = ctx.ReadValue<float>();
+
         if (value == 0) OnHoldCancel();
-
-        if (heldTimer != null) return;
-
-        heldTimer = StartCoroutine(HeldTimer());
-        onHoldBegin.Invoke();
+        
+        if (value > 0)
+        {
+            if (heldTimer != null) return;
+            heldTimer = StartCoroutine(HeldTimer());
+            onHoldBegin.Invoke();
+            StartLerpingValue();
+        }
     }
 
     private void OnHoldEnd()
     {
         performed.Invoke();
         onHoldEnd.Invoke();
+
+        StopLerpingValue();
     }
 
     private void OnHoldCancel()
     {
         StopCoroutine(heldTimer);
         heldTimer = null;
+
         onHoldCancel.Invoke();
+        StopLerpingValue();
+    }
+
+    private void StartLerpingValue()
+    {
+        if (valueCoroutine != null) StopLerpingValue();
+        valueCoroutine = StartCoroutine(LerpValue(0, 100, holdTime));
+    }
+
+    private void StopLerpingValue()
+    {
+        if (valueCoroutine != null)
+        {
+            StopCoroutine(valueCoroutine);
+            valueCoroutine = null;
+        }
+        currentValue = 0;
+    }
+
+    private IEnumerator LerpValue(float min,float max,float timeToMove)
+    {
+        var t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / timeToMove;
+            currentValue = Mathf.Lerp(min, max, t);
+            yield return null;
+        }
+        currentValue = 0f;
     }
 }
